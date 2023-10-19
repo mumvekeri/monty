aaeca5059daf79dfcd038745034e1201b2941354
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-19 14:54:41 -0700

    swap

---
+++ 6-try.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * exchange - exchanges the top two nodes of the stack
+ * @nodes: pointer to the stack
+ * @line: file line number
+ * Return: void
+ */
+
+void exchange(stack_t **nodes, unsigned int line)
+{
+	stack_t *first = *nodes;
+	stack_t *second;
+	
+	if (!first || !first->next)
+	{
+		fprintf(stderr, "L%i: can't exchange, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+	second = first->next;
+	first->next = second->next;
+	if (second->next)
+	{
+		second->next->prev = first;
+	}
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	
+	*nodes = second;
+}
+
