7f80568cbb6d82fec0395a8f9724d8032d369323
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-19 06:29:47 -0700

    try

---
+++ maybe.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+ * pall - prints all nodes
+ * @stack: nodes
+ * @line_number: file line number
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+
+	while (*stack)
+	{
+		printf("%i\n", (*stack)->n);
+		stack = &(*stack)->next;
+	}
+}
+
+/**
+ * pint - prints a node
+ * @stack: nodes
+ * @line_number: file line number
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !stack)
+	{
+		fprintf(stderr, "L%i: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%i\n", (*stack)->n);
+}
+
+/**
+ * nop -  doesn’t do anything.
+ * @stack: nodes
+ * @line_number: file line number
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
