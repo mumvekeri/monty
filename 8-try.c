2133214ff0230e5e7794b208e6cbcf1e60c58892
mumvekeri <mumvekerichidochashe@gmail.com>
2023-10-19 15:13:23 -0700

    swap

---
+++ 8-try.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * add_node - adds a node at the top of the stack
+ * @top: pointer to the top node of the stack
+ * @line_num: line number of the instruction in the file
+ */
+
+void add_node(stack_t **top, unsigned int line_num)
+{
+	char **split_buffer;
+	int value;
+	stack_t *new;
+
+	split_buffer = _split(buff, " ");
+
+	if (split_buffer == NULL)
+	{
+		fprintf(stderr, "Error: split failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new = malloc(sizeof(stack_t));
+
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(split_buffer);
+		exit(EXIT_FAILURE);
+	}
+	value = _atoi(split_buffer[1], line_num);
+
+	new->n = value;
+	new->prev = NULL;
+	new->next = *top;
+
+	if (*top)
+		(*top)->prev = new;
+	*top = new;
+	free(split_buffer);
+}
+
